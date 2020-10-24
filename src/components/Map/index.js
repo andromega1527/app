0269965307a3ffd2770a916a90f35641787539f6
Leonam <andrejc2008@hotmail.com>
2020-10-24 16:21:30 -0300

    add components

---
+++ src/components/Map/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import MapView from 'react-native-maps';
+import { View } from 'react-native';
+
+export default class Map extends Component {
+    render() {
+        return (
+            <View style={{ flex: 1 }}>
+                <MapView 
+                    style={{ flex: 1 }}
+                    region={{
+                        latitude: -27.210753,
+                        longitude: -49.644183,
+                        latitudeDelta: 0.0143,
+                        longitudeDelta: 0.0134
+                    }}
+                    showsUserLocation
+                    loadingEnabled
+                />
+            </View>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,24 +1,6 @@
 import React from 'react';
-import MapView from 'react-native-maps';
+import Map from './components/Map'
 
-import { View, Text } from 'react-native';
-
-const App = () => (
-    <View style={{ flex: 1 }}>
-        <MapView 
-            style={{ flex: 1 }}
-            region={{
-                latitude: 37.78825,
-                longitude: -122.4324,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421
-            }}
-            showsUserLocation
-            loadingEnabled
-        />
-
-        <Text>SLAaa</Text>
-    </View>
-);
+const App = () => <Map/>;
 
 export default App;
